f77b3ec4654e57e95478b8588d1dee307e6c9ce6
Melsen <48148249+melsentan99@users.noreply.github.com>
2019-11-11 23:47:40 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,27 @@
 //     return view('about' , ['tentang' => $nama]);
 // });
 
+
 Route::get('/', 'PagesController1@home');
 Route::get('/about', 'PagesController1@about');
 
+//untuk booking
 Route::get('/booking', 'BookingController@index');
+
+//BOOKING DATA
+// Route::get('/orders', 'OrderanController@index');
+// Route::get('/orders/create', 'OrderanController@create');
+// Route::get('/orders/{order}', 'OrderanController@show');
+// Route::post('/orders', 'OrderanController@store');
+// Route::delete('/orders/{order}', 'OrderanController@destroy');
+// Route::get('/orders/{order}/edit', 'OrderanController@edit');
+// Route::patch('/orders/{order}', 'OrderanController@update');
+Route::resource('orders', 'OrderanController');
+
+
+Route::get('/add', 'AdminController@index');
+Route::get('/add', 'AdminController@create');
+
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
